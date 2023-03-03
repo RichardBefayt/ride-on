5f9f50910f85f0c09b9af8d5021dfe10e4c0f654
Richard Befayt <you@example.comsprichie.rb@gmail.com>
2023-03-03 14:27:31 +0100

    Améliorations Home et About, remplissage des autres pages

--- js/script.js
+++ js/script.js
@@ -1,7 +1,5 @@
 // Définition variables
-const navbar = document.querySelector('.header .navbar');
-const background = document.querySelector('.bg-home');
-
+const navbar = document.querySelector('.background .header .navbar');
 
 // Évènements
 document.querySelector('#menu-btn').onclick = () => {
@@ -12,8 +10,15 @@ window.onscroll = () => {
    navbar.classList.remove('active');
 }
 
+document.querySelectorAll('input[type="number"]').forEach(inputNumber => {
+   inputNumber.oninput = () => {
+       if(inputNumber.value.length > inputNumber.maxLength) inputNumber.value = inputNumber.value.slice(0, inputNumber.maxLength);
+   };
+});
+
 
 // ***** Planning ***** //
+
 
 // ***** AOS ***** //
 AOS.init({
